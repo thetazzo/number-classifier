9838e893a4743012457b6c53ec5307581f354abc
pwnker <tilen.okretic@gmail.com>
2024-05-15 10:25:59 +0200

    [classifier.c] feat: testing on provided image

--- classifier.c
+++ classifier.c
@@ -30,7 +30,8 @@ char *pop_argv(int *argc, char ***argv)
 // =============================================================================
 // Neural network constants
 // =============================================================================
-size_t arch[] = {28*28, 16, 16, 2}; // this specifies the architecture of the neural network
+size_t arch[] = {28*28, 16, 16, 9, 9, 2}; // this specifies the architecture of the neural network
+float rate = 0.5f; // neural network learning rate
 
 typedef struct {
     int width;
@@ -91,13 +92,13 @@ Image raw_img_to_img(RawImage rimg)
 // =============================================================================
 void add_image_training_data(NF_Mat td, RawImage rimg, size_t index)
 {
-    size_t ind = index
+    size_t ind = index;
     for (int y = 0; y < rimg.height; ++y) {
         for (int x = 0; x < rimg.width; ++x) {
             size_t i = y*rimg.width + x;
             NF_MAT_AT(td, ind, i) = rimg.data[i]/255.f;
             for (size_t k = 1; k <= 2; ++k) {
-                if ((k-1) == ind) {
+                if (k == (ind+1)) {
                     NF_MAT_AT(td, ind, i+k) = 1;
                 } else {
                     NF_MAT_AT(td, ind, i+k) = 0;
@@ -114,7 +115,7 @@ int main(int argc, char **argv)
 
     if (argc == 0) {
         printf("[ERROR]: path to image 1 was not provided\n");
-        printf("usage: %s <img1_path> <img2_path>\n", program);
+        printf("usage: %s <img1_path> <img2_path> <test_img_path>\n", program);
         return 1;
     }
 
@@ -123,12 +124,21 @@ int main(int argc, char **argv)
 
     if (argc == 0) {
         printf("[ERROR]: path to image 2 was not provided\n");
-        printf("usage: %s <img1_path> <img2_path>\n", program);
+        printf("usage: %s <img1_path> <img2_path> <test_img_path>\n", program);
         return 1;
     }
 
     // Read image 2 path
     char *img2_file_path = pop_argv(&argc, &argv);
+
+    if (argc == 0) {
+        printf("[ERROR]: path to testing image was not provided\n");
+        printf("usage: %s <img1_path> <img2_path> <test_img_path>\n", program);
+        return 1;
+    }
+
+    // Read testing image path
+    char *test_img_file_path = pop_argv(&argc, &argv);
 
     // neural network setup
     NF_NN nn = nf_nn_alloc(NULL, arch, ARRAY_LEN(arch));
@@ -136,13 +146,15 @@ int main(int argc, char **argv)
 
     RawImage rimg1 = load_8bit_image_as_raw(img1_file_path);
     RawImage rimg2 = load_8bit_image_as_raw(img2_file_path);
+    RawImage rimg3 = load_8bit_image_as_raw(test_img_file_path);
 
-    NF_Mat td = nf_mat_alloc(NULL, 9, NF_NN_INPUT(nn).cols + NF_NN_OUTPUT(nn).cols);
+    NF_Mat td = nf_mat_alloc(NULL, 2, NF_NN_INPUT(nn).cols + NF_NN_OUTPUT(nn).cols);
 
     add_image_training_data(td, rimg1, 0);
     add_image_training_data(td, rimg2, 1);
 
     InitWindow(WIDTH, HEIGHT, "Number Classifier");
+    Font font = LoadFont("./assets/fonts/iosevka-term-ss02-regular.ttf");
 
     size_t img_width = 28;
 
@@ -154,7 +166,9 @@ int main(int argc, char **argv)
     Image img2 = raw_img_to_img(rimg2);
     Texture2D img2_texture = LoadTextureFromImage(img2);
 
-    float rate = 0.05f;
+    // preapre image 3
+    Image img3 = raw_img_to_img(rimg3);
+    Texture2D img3_texture = LoadTextureFromImage(img3);
 
     size_t epoch = 0;
 
@@ -164,6 +178,9 @@ int main(int argc, char **argv)
     float cost = 0.f;
     NUI_Plot cost_plot = {0};
 
+    char ruf[2][256]; // result string buffer
+    snprintf(ruf[0], sizeof(ruf[0]), "");
+    snprintf(ruf[1], sizeof(ruf[1]), "");
     while (!WindowShouldClose()) {
         // neural network training starts here
         nf_batch_process(&temp, &batch, batch_size, nn, td, rate);
@@ -172,6 +189,21 @@ int main(int argc, char **argv)
             da_append(&cost_plot, batch.cost);
             cost = batch.cost;
             nf_mat_shuffle_rows(td);
+        }
+        // testing occurs when `T` is pressed
+        if (IsKeyPressed(KEY_T)) {
+            for (int y = 0; y < rimg3.height; y++) {
+                for (int x = 0; x < rimg3.width; x++) {
+                    int i = y*rimg3.width + x;
+                    NF_MAT_AT(NF_NN_INPUT(nn), 0, i) = rimg3.data[i]/255.f;
+                }
+            }
+            nf_nn_forward(nn);
+            float out_0 = NF_MAT_AT(NF_NN_OUTPUT(nn), 0, 0);
+            float out_1 = NF_MAT_AT(NF_NN_OUTPUT(nn), 0, 1);
+
+            snprintf(ruf[0], sizeof(ruf[0]), "0: %f", out_0);
+            snprintf(ruf[1], sizeof(ruf[1]), "1: %f", out_1);
         }
         // Application rendering starts here
         size_t w = GetScreenWidth();
@@ -182,9 +214,18 @@ int main(int argc, char **argv)
         ClearBackground(nui_background_color());
         nui_layout_begin(NLO_HORZ, root, 3, 0);
         NUI_Rect isr = nui_layout_slot();
-        DrawTextureEx(img1_texture, CLITERAL(Vector2){isr.x+isr.w/16, isr.y+isr.h/3}, 0, scale, WHITE);
-        DrawTextureEx(img2_texture, CLITERAL(Vector2){isr.x+isr.w/16+img_width*scale, isr.y+isr.h/3}, 0, scale, WHITE);
+        // draw test results
+        Vector2 txt1_pos = CLITERAL(Vector2){isr.x + isr.w/3, isr.y+isr.h-20*scale};
+        Vector2 txt2_pos = CLITERAL(Vector2){isr.x + isr.w/3, isr.y+isr.h-15*scale};
+        DrawTextEx(font, ruf[0], txt1_pos, h*0.04f, 0.25f, WHITE); 
+        DrawTextEx(font, ruf[1], txt2_pos, h*0.04f, 0.25f, WHITE); 
+        // draw trainig images
+        DrawTextureEx(img1_texture, CLITERAL(Vector2){isr.x+isr.w/16, isr.y+isr.h/4}, 0, scale, WHITE);
+        DrawTextureEx(img2_texture, CLITERAL(Vector2){isr.x+isr.w/16+img_width*scale, isr.y+isr.h/4}, 0, scale, WHITE);
+        DrawTextureEx(img3_texture, CLITERAL(Vector2){isr.x+isr.w/16+img_width*scale/2, isr.y+isr.h/4+img_width*scale}, 0, scale, WHITE);
+        // draw cost plot
         nui_plot(cost_plot, nui_layout_slot());
+        // draw neural network
         nui_render_nn(nn, nui_layout_slot());
         nui_layout_end();
         EndDrawing();
