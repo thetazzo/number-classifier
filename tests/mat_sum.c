d5344f1e872f3b6443f75fc81fa01dc28739e9c9
pwnker <tilen.okretic@gmail.com>
2024-05-19 15:46:24 +0200

    [tests] feat: `mat_sum`

--- tests/mat_sum.c
+++ tests/mat_sum.c
@@ -1,6 +1,26 @@
-#include <stdio.h>
+#define NF_IMPLEMENTATION
+#include "../libs/nf.h"
+
 int main(void)
 {
-    return 0;
+    NF_Mat m1 = nf_mat_alloc(NULL, 4, 2);
+    NF_MAT_AT(m1, 0, 0) = 34;
+    NF_MAT_AT(m1, 1, 1) = 20;
+    NF_MAT_AT(m1, 2, 0) = 3;
+    NF_MAT_AT(m1, 3, 1) = 2;
+
+    NF_Mat m2 = nf_mat_alloc(NULL, 4, 2);
+    NF_MAT_AT(m2, 0, 0) = 35;
+    NF_MAT_AT(m2, 1, 1) = 400;
+    NF_MAT_AT(m2, 2, 0) = 3;
+    NF_MAT_AT(m2, 3, 1) = 40;
+
+    nf_mat_sum(m1, m2);
+
+    int valid = 0;
+
+    NF_MAT_PRINT(m1);
+
+    return valid;
 }
 
