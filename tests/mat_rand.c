f3b7c33720490ba9094b5f8bc3a8d1a2ab62136d
pwnker <tilen.okretic@gmail.com>
2024-05-20 23:12:46 +0200

    [tests] update: re-recorded output

--- tests/mat_rand.c
+++ tests/mat_rand.c
@@ -9,18 +9,8 @@ int main()
     NF_Mat prob2 = nf_mat_alloc(NULL, 4, 4);
     nf_mat_rand(prob2, -1 , 1);
 
-    size_t count = 0;
-    for (size_t i = 0; i < 4; ++i) {
-        for (size_t j = 0; j < 4; ++j) {
-            float pv1 = NF_MAT_AT(prob1, i, j);
-            float pv2 = NF_MAT_AT(prob2, i, j);
-            if (pv1 == pv2) {
-                count += 1;
-            }
-        }
-    }
-    if (count == 16) {
-        return 1;
-    }
+    NF_MAT_PRINT(prob1);
+    printf("--------------------------------------------------\n");
+    NF_MAT_PRINT(prob2);
     return 0;
 }
